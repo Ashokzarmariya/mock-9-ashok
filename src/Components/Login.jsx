a886710bb1b8d221e16f79813195c35f64616151
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-08-23 14:33:53 +0530

    dummy page complated

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { useNavigate } from "react-router-dom";
+import "./Signup.css";
 
 const Login = () => {
-  return (
-    <div>
-      this is login page
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setUserData((values) => ({ ...values, [name]: value }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("data", userData);
+  };
+  return (
+    <div className="container">
+      <div className="mainDiv">
+        <form className="" onSubmit={handleSubmit}>
+          <div className="">
+            <p className="lables">Email</p>
+            <TextField
+              onChange={handleChange}
+              value={userData.email}
+              name="email"
+              className="inputs"
+              required
+              placeholder="your Email"
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+            />
+          </div>
+
+          <div className="">
+            <p className="labels">Password</p>
+            <TextField
+              onChange={handleChange}
+              value={userData.password}
+              name="password"
+              className="inputs"
+              type="password"
+              required
+              placeholder="Secret Password"
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+            />
+          </div>
+
+          <div>
+            <input
+              className="submitButton"
+              type="submit"
+              value={"Login"}
+            />
+          </div>
+        </form>
+        <div onClick={() => navigate("/signup")} className="already">
+          <p>Not Have Account ?</p>
+          <p className="togalButton">signup</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,11 +1,91 @@
-import React from 'react'
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { useNavigate } from "react-router-dom";
+import "./Signup.css";
 
 const Signup = () => {
-  return (
-    <div>
-      this is signup page
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    email: "",
+    name: "",
+    password: "",
+  });
 
-export default Signup
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setUserData((values) => ({ ...values, [name]: value }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("data", userData);
+  };
+  return (
+    <div className="container">
+      <div className="mainDiv">
+        <form className="" onSubmit={handleSubmit}>
+          <div className="">
+            <p className="labels">Name</p>
+            <TextField
+              onChange={handleChange}
+              value={userData.name}
+              name="name"
+              className="inputs"
+              type="text"
+              required
+              placeholder="Your Name"
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+            />
+          </div>
+
+          <div className="">
+            <p className="lables">Email</p>
+            <TextField
+              onChange={handleChange}
+              value={userData.email}
+              name="email"
+              className="inputs"
+              required
+              placeholder="your Email"
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+            />
+          </div>
+
+          <div className="">
+            <p className="labels">Password</p>
+            <TextField
+              onChange={handleChange}
+              value={userData.password}
+              name="password"
+              className="inputs"
+              type="password"
+              required
+              placeholder="Secret Password"
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+            />
+          </div>
+
+          <div>
+            <input
+              className="submitButton"
+              type="submit"
+              
+              value={"Sign Up"}
+            />
+          </div>
+        </form>
+        <div onClick={() => navigate("/login")} className="already">
+          <p>If You Have Account ?</p>
+          <p className="togalButton">Login</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
